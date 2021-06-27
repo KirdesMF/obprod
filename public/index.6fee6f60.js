69b3e752a387080facd7bb3145ef21c4ac68cb95
Cedric Gourville <cedgourville@gmail.com>
2021-06-27 03:00:07 +0200

    fix script js

---
+++ public/index.6fee6f60.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelectorAll('[data-anime="list"] > li'),t=gsap.timeline({repeat:-1});e.forEach((function(e){t.to(e,{y:0,duration:2,ease:Expo.easeout}),t.to(e,{y:100,delay:1,duration:1})}))}();
+//# sourceMappingURL=index.6fee6f60.js.map
