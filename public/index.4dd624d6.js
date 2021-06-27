7a9a414f587db96aabacc7be2d7cb25189f49ddf
Cedric Gourville <cedgourville@gmail.com>
2021-06-27 02:52:40 +0200

    deploy

---
+++ public/index.4dd624d6.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll('[data-anime="list"] > li'),a=gsap.timeline({repeat:-1});e.forEach((function(e){a.to(e,{y:0,duration:2,ease:Expo.easeout}),a.to(e,{y:100,delay:1,duration:1})}));export{$7a4be64d348747d761442c05b15be42d$exports as default};
+//# sourceMappingURL=index.4dd624d6.js.map
